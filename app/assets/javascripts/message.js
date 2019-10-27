be301cf83c82a83ed5097e025b866d3b434c6c4e
takao8989 <56573708+takao8989@users.noreply.github.com>
2019-10-27 21:05:36 +0900

    create_ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,10 +35,6 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('.textbox').val('');
       $('form')[0].reset();
     })
     .fail(function(){

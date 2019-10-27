d83d6c1ffbb17bdb06109994e7199b4ec6fff8c0
takao8989 <56573708+takao8989@users.noreply.github.com>
2019-10-27 20:08:37 +0900

    create_ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
+    var image = ( message.image ) ? `<img class= "lower-message__image" src=${message}>` : "";
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -16,31 +16,12 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
+         ${image}
        </div>`
      return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+   } 
 
-$(function(){
+  $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -64,6 +45,6 @@ $(function(){
       alert('error');
     });
     return false;
+    });
   });
-});
 });

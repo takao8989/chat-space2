3b3ff65a291b5e87ac1c8d120c60f7eb024022ff
takao8989 <56573708+takao8989@users.noreply.github.com>
2019-10-29 16:47:28 +0900

    create-auto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-    var image = ( message.image ) ? `<img class= "lower-message__image" src=${message}>` : "";
+    var image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image}>` : "";
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -46,4 +46,27 @@ $(function(){
     return false;
     });
   });
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      last_message_id = $('.message:last').data('message-id');
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  };
+  setInterval(reloadMessages, 5000);
 });
